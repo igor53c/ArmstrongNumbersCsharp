08703ed5c881aa7e1c053b31a0665f625b754e8a
Igor <igor53c@gmail.com>
2024-05-18 13:13:21 +0200

    Add project files.

---
+++ ArmstrongNumbers/ArmstrongNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArmstrongNumbers
+{
+    public static class ArmstrongNumbers
+    {
+        public static bool IsArmstrongNumber(int numberToCheck)
+        {
+            string numberStr = numberToCheck.ToString();
+            int numberOfDigits = numberStr.Length;
+            int sum = 0;
+            int temp = numberToCheck;
+
+            while (temp != 0)
+            {
+                int digit = temp % 10;
+                sum += (int)Math.Pow(digit, numberOfDigits);
+                temp /= 10;
+            }
+
+            return sum == numberToCheck;
+        }
+    }
+}
---
+++ ArmstrongNumbers/ArmstrongNumbersTests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+
+namespace ArmstrongNumbers.Tests
+{
+    public class ArmstrongNumbersTests
+    {
+        [Fact]
+        public void TestSingleDigitNumbers()
+        {
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(0));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(1));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(2));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(3));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(4));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(5));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(6));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(7));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(8));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(9));
+        }
+
+        [Fact]
+        public void TestDoubleDigitNumbers()
+        {
+            Assert.False(ArmstrongNumbers.IsArmstrongNumber(10));
+            Assert.False(ArmstrongNumbers.IsArmstrongNumber(11));
+            Assert.False(ArmstrongNumbers.IsArmstrongNumber(99));
+        }
+
+        [Fact]
+        public void TestKnownArmstrongNumbers()
+        {
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(153));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(370));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(371));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(407));
+            Assert.True(ArmstrongNumbers.IsArmstrongNumber(9474));
+        }
+
+        [Fact]
+        public void TestKnownNonArmstrongNumbers()
+        {
+            Assert.False(ArmstrongNumbers.IsArmstrongNumber(9475));
+            Assert.False(ArmstrongNumbers.IsArmstrongNumber(100));
+            Assert.False(ArmstrongNumbers.IsArmstrongNumber(123));
+            Assert.False(ArmstrongNumbers.IsArmstrongNumber(400));
+        }
+    }
+}
